190de97bee42e6a9a1c2172829161046e44583e8
Sergey Popyvanov <s.popyvanov@gmail.com>
2025-10-24 20:46:52 +0400

    Добавлена документация для C interop механизма (shim.h и module.modulemap)
    - Объяснена роль module.modulemap в импорте C-библиотек в Swift
    - Документирована структура module map (атрибуты, директивы)
    - Объяснено назначение shim.h как промежуточного заголовка
    - Добавлены ссылки на официальную документацию Swift и Clang
    
    Это упрощает понимание архитектуры для будущих разработчиков
    и помогает при добавлении новых C-библиотек в проект.

--- Sources/CTDLib/shim.h
+++ Sources/CTDLib/shim.h
@@ -1,2 +1,27 @@
+/**
+ * Shim header для экспорта TDLib C API в Swift
+ *
+ * Что такое module.modulemap?
+ * Module map - это файл, который описывает, как C/C++/Objective-C код должен быть
+ * импортирован как Swift-модуль. Он связывает C-заголовки с Swift-модулями.
+ * Без module.modulemap Swift не знает, как импортировать C-библиотеки.
+ *
+ * Зачем нужен shim.h?
+ * Shim header (промежуточный заголовок) - это паттерн, где мы создаём один заголовочный
+ * файл, который включает все нужные C-заголовки. Преимущества:
+ * 1. Централизованное управление импортами - все #include в одном месте
+ * 2. Возможность платформо-специфичных импортов через макросы (#if __APPLE__)
+ * 3. Избежание абсолютных путей в module.modulemap (улучшает портативность)
+ * 4. Упрощение module.modulemap - он ссылается только на shim.h
+ *
+ * Импортируемые заголовки TDLib:
+ * - td_json_client.h: JSON-based API для работы с Telegram (send/receive/execute)
+ * - td_log.h: API для настройки логирования TDLib
+ *
+ * См. также:
+ * - module.modulemap: определяет Swift-модуль CTDLib
+ * - https://www.swift.org/documentation/articles/wrapping-c-cpp-library-in-swift.html
+ */
+
 #include <td/telegram/td_json_client.h>
 #include <td/telegram/td_log.h>
